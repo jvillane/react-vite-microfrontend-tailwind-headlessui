4c1126542fb7aab06f74211a3c320835ebc3d376
Fabian Mosso <fabian.mosso@guou.cl>
2024-08-02 12:14:46 -0400

    feature: levantar local

--- frontend/root/src/App.tsx
+++ frontend/root/src/App.tsx
@@ -1,3 +1,5 @@
+
+
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -16,7 +18,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Root: Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
