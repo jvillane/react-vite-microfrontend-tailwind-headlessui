1b8c31eeb70d737c377cc6e21ace8ce56ec057be
Fabian Mosso <fabian.mosso@guou.cl>
2024-08-06 13:03:00 -0400

    added cognito

--- frontend/root/src/App.tsx
+++ frontend/root/src/App.tsx
@@ -1,41 +1,32 @@
 
 
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Amplify } from "aws-amplify"
+import { RouterProvider } from 'react-router-dom';
 import './App.css'
+import React from "react";
+import router from "./router";
+const envVariable = import.meta.env
+
+Amplify.configure({
+  Auth:{
+    Cognito:{
+      userPoolId: envVariable.VITE_USER_POOL_ID,
+      userPoolClientId: envVariable.VITE_USER_POOL_CLIENT_ID,
+      identityPoolId: envVariable.VITE_USER_POOL_ID,
+      signUpVerificationMethod:"code"
+    }
+  }
+})
+
 
 function App() {
-  const [count, setCount] = useState(0)
-  const envVariable = import.meta.env
+
+
   return (
     <>
-      <div>
-        <div>
-          <h1>VITE_PUBLIC_VARIABLE: {envVariable.VITE_PUBLIC_VARIABLE} </h1>
-          <h1>PUBLIC_VARIABLE: {envVariable.PUBLIC_VARIABLE} </h1>
-          <h1>VITE_SECRET_VARIABLE: {envVariable.VITE_SECRET_VARIABLE} </h1>
-          <h1>SECRET_VARIABLE: {envVariable.SECRET_VARIABLE} </h1>
-        </div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Root: Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <React.StrictMode>
+        <RouterProvider router={router} />
+      </React.StrictMode>
     </>
   )
 }
